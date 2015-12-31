994af2e14b7d82bca677aca4392887f7e6ca73d7
a59566 <a59566@outlook.com>
2016-01-01 00:27:38 +0800

    prepare code for dark chess game

--- mine.c
+++ mine.c
@@ -5,9 +5,7 @@
  */
 struct block
 {
-    gint count; /* 周圍有多少地雷 */
-    gboolean mine; /* 是否藏有地雷 */
-    gboolean marked; /* 是否被標記過 */
+    gint color;     //白色1 黑色2
     gboolean opened; /* 是否已被掀開 */
     GtkWidget *button;
 };
@@ -15,17 +13,18 @@ struct block
 /**
  * 全域變數
  */
-static struct block *map; /* 地雷區資料 */
-static gint width = 10; /* 地雷區寬度 */
-static gint height = 10; /* 地雷區高度 */
-static gint mines = 20; /* 地雷數量 */
+static struct block *map; /* 棋盤區資料 */
+static gint width = 10; /* 棋盤區寬度 */
+static gint height = 10; /* 棋盤區高度 */
 
-static GtkWidget *mine_label; /* 顯示剩餘地雷數 */
+static gint white_chess = 16;   //黑色棋子數量
+static gint black_chess = 16;   //白色棋子數量
+
+static GtkWidget *white_chess_label; /* 顯示剩餘白色數量 */
 static GtkWidget *time_label; /* 顯示遊戲時間 */
 static gint button_size = 20; /* button 大小 */
 
 static gint opened_count; /* 已經掀開多少格子 */
-static gint marked_count; /* 已經標記多少格子 */
 static gboolean game_over; /* 遊戲是否已結束 */
 
 static GtkWidget *window; /* 主視窗 */
@@ -55,57 +54,42 @@ void game_init()
 
     /* 初始變數值 */
     opened_count = 0;
-    marked_count = 0;
     game_over = FALSE;
     game_time = 0;
-
-    /* 顯示剩餘地雷數 */
+    white_chess = 16;
+    black_chess = 16;
+    
+    /* 顯示剩餘白色數量*/
     gchar buf[4];
-    g_snprintf(buf, 4, "%d", MAX(0, mines - marked_count));
-    gtk_label_set_text(GTK_LABEL(mine_label), buf);
+    g_snprintf(buf, 4, "%d", white_chess);
+    gtk_label_set_text(GTK_LABEL(white_chess_label), buf);
 
-    /* 以亂數安置地雷 */
+    /* 以亂數安置白色棋子*/
     gint size = width * height;
     gint i = 0;
-    while (i < mines)
+    while (i < white_chess)
     {
         gint index;
-        gint row, col;
         index = g_random_int_range(0, size);
-        if (map[index].mine == TRUE) /* 已有地雷 */
+        if (map[index].color == 1 || map[index].color == 2)
             continue;
-        map[index].mine = TRUE;
-        row = index / width;
-        col = index % width;
-
-        /* 四周格子的 count 加一 */
-        if (row > 0)
-        {
-            /* 左上 */if (col > 0)
-                map[index - width - 1].count++;
-            /* 正上 */
-            map[index - width].count++;
-            /* 右上 */if (col < width - 1)
-                map[index - width + 1].count++;
-        }
-        /* 左 */
-        if (col > 0)
-            map[index - 1].count++;
-        /* 右 */
-        if (col < width - 1)
-            map[index + 1].count++;
-        if (row < height - 1)
-        {
-            /* 左下 */if (col > 0)
-                map[index + width - 1].count++;
-            /* 正下 */
-            map[index + width].count++;
-            /* 右下 */if (col < width - 1)
-                map[index + width + 1].count++;
-        }
-
+        map[index].color = 1;
+        
         i++;
     }
+     /* 以亂數安置黑色棋子*/
+    i = 0;
+    while (i < black_chess)
+    {
+        gint index;
+        index = g_random_int_range(0, size);
+        if (map[index].color == 1 || map[index].color == 2)
+            continue;
+        map[index].color = 2;
+        
+        i++;
+    }
+    
 
     /* 啟動計時 */
     g_timeout_add(1000, (GSourceFunc) tick, NULL);
@@ -124,27 +108,7 @@ void gameover(gboolean won)
 
     game_over = TRUE;
 
-    if (won == TRUE)   /* 準備掃雷成功的訊息 */
-    {
-        g_snprintf(msg, 100, "You won! You have cleared"
-                   " the game in %3d seconds.", game_time);
-    }
-    else     /* 準備踩到地雷的訊息 */
-    {
-        // === show mine ===
-        int i, n;
-        n = width * height;
-        for (i = 0; i < n; i++)
-        {
-            if (map[i].mine == TRUE)
-            {
-                gtk_button_set_label(GTK_BUTTON(map[i].button), "*");
-            }
-        }
-        // === show mine ===
-        g_snprintf(msg, 100, "Bad luck. Game over.");
-    }
-
+    
     /* 用 GtkMessageDialog 顯示訊息 */
     dialog = gtk_message_dialog_new(GTK_WINDOW(window), 0, GTK_MESSAGE_INFO,
                                     GTK_BUTTONS_OK, msg);
@@ -163,9 +127,7 @@ void open_block(gint x, gint y)
 
     index = x + y * width;
 
-    /* 遊戲已結束或防止玩家誤翻有記號的格子 */
-    if (map[index].marked == TRUE)
-        return;
+   
 
     button = map[index].button;
 
@@ -180,52 +142,25 @@ void open_block(gint x, gint y)
 
     map[index].opened = TRUE; /* 格子狀態為掀開 */
 
-    if (map[index].mine == TRUE)  /* 若藏有地雷 */
-    {
-        gtk_button_set_label(GTK_BUTTON(button), "*");
-        gameover(FALSE);/* 踩到地雷遊戲結束 */
-        return;
-    }
-
-    if (map[index].count > 0)   /* 若周圍有地雷 */
+    if (map[index].color == 1 || map[index].color == 2)   /* 暫時先這樣弄 之後只會有32格 每格必有棋子 */
     {
         gchar buf[2];
-        g_snprintf(buf, 2, "%d", map[index].count);
+        g_snprintf(buf, 2, "%d", map[index].color);
         gtk_button_set_label(GTK_BUTTON(button), buf);
     }
 
-    opened_count++; /* 已掀開的格子又多了一個 */
+    
 
+    opened_count++; /* 已掀開的格子又多了一個 */
+    
+    /*
     if (opened_count + mines == width * height)
     {
-        gameover(TRUE); /* 所有空地都被翻完時遊戲結束 */
+        gameover(TRUE);  所有空地都被翻完時遊戲結束 
         return;
-    }
+    }*/
 
-    if (map[index].count == 0)  /* 若周圍沒有地雷 */
-    {
-        /* 掀開周圍格子 */
-        if (y > 0)
-        {
-            if (x > 0)
-                open_block(x - 1, y - 1);
-            open_block(x, y - 1);
-            if (x < width - 1)
-                open_block(x + 1, y - 1);
-        }
-        if (x > 0)
-            open_block(x - 1, y);
-        if (x < width - 1)
-            open_block(x + 1, y);
-        if (y < height - 1)
-        {
-            if (x > 0)
-                open_block(x - 1, y + 1);
-            open_block(x, y + 1);
-            if (x < width - 1)
-                open_block(x + 1, y + 1);
-        }
-    }
+    
 }
 
 /**
@@ -264,26 +199,12 @@ int on_mouse_click(GtkWidget *widget, GdkEventButton *event, gpointer data)
         break;
     case 2: /* 滑鼠中鍵 */
         break;
-    case 3: /* 滑鼠右鍵 */
-        /* 已掀開的格子不做記號 */
-        if (map[index].opened == TRUE)
+    case 3: //滑鼠右鍵
             break;
-        /* 原來有記號則消掉, 沒有則畫上記號 */
-        if (map[index].marked != TRUE)
-        {
-            map[index].marked = TRUE;
-            gtk_button_set_label(GTK_BUTTON(widget), "@");
-            marked_count++;
-        }
-        else
-        {
-            map[index].marked = FALSE;
-            gtk_button_set_label(GTK_BUTTON(widget), "");
-            marked_count--;
-        }
-        /* 顯示新的地雷數 */
+       
+        /* 顯示新的地雷數 
         g_snprintf(buf, 4, "%d", MAX(0, mines - marked_count));
-        gtk_label_set_text(GTK_LABEL(mine_label), buf);
+        gtk_label_set_text(GTK_LABEL(white_chess_label), buf);*/
     }
 
     /**
@@ -316,10 +237,10 @@ int main(int argc, char **argv)
 
     /* 存放 label 的第一個 hbox */
     hbox = gtk_hbox_new(FALSE, 0);
-    label = gtk_label_new("Mines:");
+    label = gtk_label_new("white_chess:");
     gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 4);
-    mine_label = gtk_label_new("0");
-    gtk_box_pack_start(GTK_BOX(hbox), mine_label, FALSE, FALSE, 2);
+    white_chess_label = gtk_label_new("0");
+    gtk_box_pack_start(GTK_BOX(hbox), white_chess_label, FALSE, FALSE, 2);
     label = gtk_label_new("Time:");
     gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 4);
     time_label = gtk_label_new("0");
