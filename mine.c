7d8f0650a0b0df8e3c8766778c1048ec2e3a9399
a59566 <a59566@outlook.com>
2016-01-01 13:22:49 +0800

    change layout as 8x4 & delete timer

--- mine.c
+++ mine.c
@@ -14,37 +14,20 @@ struct block
  * 全域變數
  */
 static struct block *map; /* 棋盤區資料 */
-static gint width = 10; /* 棋盤區寬度 */
-static gint height = 10; /* 棋盤區高度 */
+static gint width = 8; /* 棋盤區寬度 */
+static gint height = 4; /* 棋盤區高度 */
 
 static gint white_chess = 16;   //黑色棋子數量
 static gint black_chess = 16;   //白色棋子數量
 
 static GtkWidget *white_chess_label; /* 顯示剩餘白色數量 */
-static GtkWidget *time_label; /* 顯示遊戲時間 */
-static gint button_size = 20; /* button 大小 */
+static gint button_size = 80; /* button 大小 */
 
 static gint opened_count; /* 已經掀開多少格子 */
 static gboolean game_over; /* 遊戲是否已結束 */
 
 static GtkWidget *window; /* 主視窗 */
-static gint game_time; /* 遊戲開始後的計秒 */
 
-/**
- * g_timeout_add() ? callback function
- */
-gboolean tick(gpointer data)
-{
-    gchar buf[8];
-
-    if (game_over == TRUE) /* 遊戲結束則傳回 FALSE 停止計時 */
-        return FALSE;
-
-    game_time++; /* 增加遊戲時間並更新顯示 */
-    g_snprintf(buf, 8, "%d", game_time);
-    gtk_label_set_text(GTK_LABEL(time_label), buf);
-    return TRUE; /* 傳回 TRUE 繼續計時 */
-}
 
 /**
  * game start
@@ -55,10 +38,9 @@ void game_init()
     /* 初始變數值 */
     opened_count = 0;
     game_over = FALSE;
-    game_time = 0;
     white_chess = 16;
     black_chess = 16;
-    
+
     /* 顯示剩餘白色數量*/
     gchar buf[4];
     g_snprintf(buf, 4, "%d", white_chess);
@@ -74,7 +56,7 @@ void game_init()
         if (map[index].color == 1 || map[index].color == 2)
             continue;
         map[index].color = 1;
-        
+
         i++;
     }
      /* 以亂數安置黑色棋子*/
@@ -86,13 +68,10 @@ void game_init()
         if (map[index].color == 1 || map[index].color == 2)
             continue;
         map[index].color = 2;
-        
+
         i++;
     }
-    
 
-    /* 啟動計時 */
-    g_timeout_add(1000, (GSourceFunc) tick, NULL);
 }
 
 /**
@@ -108,13 +87,6 @@ void gameover(gboolean won)
 
     game_over = TRUE;
 
-    
-    /* 用 GtkMessageDialog 顯示訊息 */
-    dialog = gtk_message_dialog_new(GTK_WINDOW(window), 0, GTK_MESSAGE_INFO,
-                                    GTK_BUTTONS_OK, msg);
-    gtk_dialog_run(GTK_DIALOG(dialog));
-    //game_init();
-    gtk_widget_destroy(dialog);
 }
 
 /**
@@ -127,7 +99,7 @@ void open_block(gint x, gint y)
 
     index = x + y * width;
 
-   
+
 
     button = map[index].button;
 
@@ -149,18 +121,18 @@ void open_block(gint x, gint y)
         gtk_button_set_label(GTK_BUTTON(button), buf);
     }
 
-    
+
 
     opened_count++; /* 已掀開的格子又多了一個 */
-    
+
     /*
     if (opened_count + mines == width * height)
     {
-        gameover(TRUE);  所有空地都被翻完時遊戲結束 
+        gameover(TRUE);  所有空地都被翻完時遊戲結束
         return;
     }*/
 
-    
+
 }
 
 /**
@@ -201,8 +173,8 @@ int on_mouse_click(GtkWidget *widget, GdkEventButton *event, gpointer data)
         break;
     case 3: //滑鼠右鍵
             break;
-       
-        /* 顯示新的地雷數 
+
+        /* 顯示新的地雷數
         g_snprintf(buf, 4, "%d", MAX(0, mines - marked_count));
         gtk_label_set_text(GTK_LABEL(white_chess_label), buf);*/
     }
@@ -216,75 +188,66 @@ int on_mouse_click(GtkWidget *widget, GdkEventButton *event, gpointer data)
 /**
  * main
  */
-int main(int argc, char **argv)
-{
-    GtkWidget *vbox;
-    GtkWidget *hbox;
-    GtkWidget *label;
-    gint i, j, index;
+int main(int argc, char **argv) {
+	GtkWidget *vbox;
+	GtkWidget *hbox;
+	GtkWidget *label;
+	gint i, j, index;
 
-    /* initialize GTK library */
-    gtk_init(&argc, &argv);
+	/* initialize GTK library */
+	gtk_init(&argc, &argv);
 
-    /* 分配記憶體給 map 並初始化 */
-    map = (struct block *) g_malloc0(sizeof(struct block) * width * height);
+	/* 分配記憶體給 map 並初始化 */
+	map = (struct block *) g_malloc0(sizeof(struct block) * width * height);
 
-    /* build the user interface */
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(G_OBJECT(window), "delete_event", gtk_main_quit, NULL);
+	/* build the user interface */
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "delete_event", gtk_main_quit, NULL);
+	gtk_window_set_default_size(GTK_WINDOW(window),640,350);
 
-    vbox = gtk_vbox_new(FALSE, 0);
+	vbox = gtk_vbox_new(FALSE, 0);
 
-    /* 存放 label 的第一個 hbox */
-    hbox = gtk_hbox_new(FALSE, 0);
-    label = gtk_label_new("white_chess:");
-    gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 4);
-    white_chess_label = gtk_label_new("0");
-    gtk_box_pack_start(GTK_BOX(hbox), white_chess_label, FALSE, FALSE, 2);
-    label = gtk_label_new("Time:");
-    gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 4);
-    time_label = gtk_label_new("0");
-    gtk_box_pack_start(GTK_BOX(hbox), time_label, FALSE, FALSE, 2);
-    gtk_widget_show_all(hbox);
-    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
+	/* 存放 label 的第一個 hbox */
+	hbox = gtk_hbox_new(FALSE, 0);
+	label = gtk_label_new("Mines:");
+	gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 4);
+	gtk_widget_show_all(hbox);
+	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
 
-    /* width x height 個 button 的區塊 */
-    for (i = 0, index = 0; i < height; i++)
-    {
-        gint j;
-        hbox = gtk_hbox_new(FALSE, 0);
-        for (j = 0; j < width; j++)
-        {
-            GtkWidget *button;
-            button = gtk_toggle_button_new();
-            gtk_widget_set_usize(button, button_size, button_size);
-            /**
-             * 設定 button 的屬性使它不會成為輸入的 focus.
-             * (否則有一個 button 上會有框, 很難看)
-             */
-            g_object_set(G_OBJECT(button), "can-focus", FALSE, NULL);
-            gtk_box_pack_start(GTK_BOX(hbox), button, FALSE, FALSE, 0);
-            gtk_widget_show(button);
+	/* width x height 個 button 的區塊 */
+	for (i = 0, index = 0; i < height; i++) {
+		gint j;
+		hbox = gtk_hbox_new(FALSE, 0);
+		for (j = 0; j < width; j++) {
+			GtkWidget *button;
+			button = gtk_toggle_button_new();
+			gtk_widget_set_usize(button, button_size, button_size);
+			/**
+			 * 設定 button 的屬性使它不會成為輸入的 focus.
+			 * (否則有一個 button 上會有框, 很難看)
+			 */
+			g_object_set(G_OBJECT(button), "can-focus", FALSE, NULL);
+			gtk_box_pack_start(GTK_BOX(hbox), button, FALSE, FALSE, 0);
+			gtk_widget_show(button);
 
-            g_signal_connect(G_OBJECT(button), "button-press-event",
-                             G_CALLBACK(on_mouse_click), (gpointer) index);
-            map[index].button = button;
-            index++;
-        }
-        gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-        gtk_widget_show(hbox);
-    }
+			g_signal_connect(G_OBJECT(button), "button-press-event",
+					G_CALLBACK(on_mouse_click), (gpointer) index);
+			map[index].button = button;
+			index++;
+		}
+		gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
+		gtk_widget_show(hbox);
+	}
 
-    gtk_container_add(GTK_CONTAINER(window), vbox);
-    gtk_widget_show(vbox);
-    gtk_widget_show(window);
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+	gtk_widget_show(vbox);
+	gtk_widget_show(window);
 
-    game_init();
+	game_init();
 
-    gtk_main();
+	gtk_main();
 
-    g_free(map); /* 釋放記憶體 */
+	g_free(map); /* 釋放記憶體 */
 
-    return 0;
+	return 0;
 }
-
